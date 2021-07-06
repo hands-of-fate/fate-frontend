b922823a130617e3274fb7f1c1044652bdb7c153
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-07-06 10:29:03 -0700

    created title page and home page

---
+++ src/LoginComp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class LoginComp extends Component {
+    render() {
+        return (
+            <div>
+                log in!
+            </div>
+        )
+    }
+}
---
+++ src/SignupComp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SignupComp extends Component {
+    render() {
+        return (
+            <div>
+                sign up!
+            </div>
+        )
+    }
+}
