e4c7b49901c0843416ce30fd9cb6dee66c7d9f52
Missael Ortiz <ortiz.missael80@gmail.com>
2021-07-07 14:19:05 -0700

    added in MinorArcanaEffects and values

--- src/CombatPage.js
+++ src/CombatPage.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
+import { getAllTypedCards } from './utils/FetchUtils'
+import Deck from 'card-deck'
 
 export default class CombatPage extends Component {
     state = {
         current_deck: []
+    }
+
+    doFetch = async () => {
+        const all_cards_data = await getAllTypedCards('minor');
+        var minorTarot = new Deck(all_cards_data);
+        this.setState({ current_deck: minorTarot.drawRandom(20)})
+    }
+
+    componentDidMount = async () => {
+        await this.doFetch();
     }
     render() {
         return (
